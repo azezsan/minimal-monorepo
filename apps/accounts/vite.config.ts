fd0c706014d479edbb496a82950fa8fd5c8e2b8c
azez <t.s.connect.m@gmail.com>
2024-10-30 18:35:23 +0300

    Revert "add ports to sveltekit dev modes"
    This reverts commit ae53f5619c62b9d10be698b2ea6a6e1e9c10852e.

--- apps/accounts/vite.config.ts
+++ apps/accounts/vite.config.ts
@@ -3,9 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: {
-		port: 5174
-	},
 	plugins: [
 		sveltekit(),
 		paraglide({
