e61f7bae5f2fc20f0f67ef4afa53c3bdcec1a5f2
azez <t.s.connect.m@gmail.com>
2024-11-01 09:30:36 +0300

    add ui to accounts

--- apps/accounts/src/routes/+layout.svelte
+++ apps/accounts/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '../app.css';
+	import '@acme/ui/app.css';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
--- apps/accounts/src/routes/login/+page.svelte
+++ apps/accounts/src/routes/login/+page.svelte
@@ -1,1 +1,5 @@
-<a href="/login/google">Sign in with Google</a>
+<script>
+	import { Button } from "@acme/ui/lib/components/ui/button";
+</script>
+
+<Button href="/login/google">Sign in with Google</Button>
