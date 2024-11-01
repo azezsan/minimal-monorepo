731500760972ba3d728f1a0353fcabcb22cae9c2
azez <t.s.connect.m@gmail.com>
2024-11-01 16:07:56 +0300

    fix rpc

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,8 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi'
 import getUser from './routes/users/[id]/get.js'
 import getAllUsers from './routes/users/get.js'
 
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+const app = new OpenAPIHono<{ Bindings: CloudflareBindings }>()
   .route('/', getUser)
   .route('/', getAllUsers)
 
--- apps/api/src/routes/users/get.ts
+++ apps/api/src/routes/users/get.ts
@@ -1,24 +1,26 @@
-import { OpenAPIHono, z } from '@hono/zod-openapi'
+import { createRoute, OpenAPIHono, z } from '@hono/zod-openapi'
 import { selectUserSchema, initializeD1, usersTable } from '@acme/db'
 
-export default new OpenAPIHono<{ Bindings: CloudflareBindings }>()
-    .openapi({
-        method: 'get',
-        path: '/users',
-        summary: 'List all users',
-        description: 'Retrieves a list of all users in the system',
-        tags: ['users'],
-        responses: {
-            200: {
-                content: {
-                    'application/json': {
-                        schema: z.array(selectUserSchema)
-                    }
-                },
-                description: 'Successfully retrieved users'
-            }
+const route = createRoute({
+    method: 'get',
+    path: '/users',
+    summary: 'List all users',
+    description: 'Retrieves a list of all users in the system',
+    tags: ['users'],
+    responses: {
+        200: {
+            content: {
+                'application/json': {
+                    schema: z.array(selectUserSchema)
+                }
+            },
+            description: 'Successfully retrieved users'
         }
-    }, async (c) => {
+    }
+})
+
+export default new OpenAPIHono<{ Bindings: CloudflareBindings }>()
+    .openapi(route, async (c) => {
         const db = initializeD1(c.env.DB) // Cloudflare D1 database
         const results = await db.select().from(usersTable).all()
         return c.json(results)
