e8a592d227990af140e60d74d299b0734daed8d9
azez <t.s.connect.m@gmail.com>
2024-12-20 16:57:57 +0300

    🧹 chore: add eslint tooling

---
+++ tooling/eslint/sveltekit.js
@@ -0,0 +1,30 @@
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import { config as baseConfig } from './base'
+
+export default ts.config(
+    ...baseConfig,
+	...svelte.configs['flat/recommended'],
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	},
+	{
+		ignores: ['build/', '.svelte-kit/', 'dist/']
+	}
+);
