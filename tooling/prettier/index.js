a1d094249d9d7a194a6f7f82d8c186a4699e64ad
azez <t.s.connect.m@gmail.com>
2024-12-28 17:35:46 +0300

    🧹 chore: prettier tooling

---
+++ tooling/prettier/index.js
@@ -0,0 +1,53 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-svelte").PluginConfig} SvelteConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig | SvelteConfig } */
+const config = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-svelte",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindFunctions: ["cn", "cva"],
+//   importOrder: [
+//     "<TYPES>",
+//     "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+//     "^(next/(.*)$)|^(next$)",
+//     "^(expo(.*)$)|^(expo$)",
+//     "<THIRD_PARTY_MODULES>",
+//     "",
+//     "<TYPES>^@acme",
+//     "^@acme/(.*)$",
+//     "",
+//     "<TYPES>^[.|..|~]",
+//     "^~/",
+//     "^[../]",
+//     "^[./]",
+//   ],
+//   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.7.0",
+  overrides: [
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+      },
+    },
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
+};
+
+export default config;
