cb0f971e7b43d3956cc88947625a034beab0f866
azez <t.s.connect.m@gmail.com>
2024-12-28 16:54:09 +0300

    🧹 chore: update eslint

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,33 +1,50 @@
-// @ts-nocheck
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import ts from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+/// <reference types="./types.d.ts" />
 
-/**
- * A shared ESLint configuration for the repository.
- *
- * @type {import("eslint").Linter.Config}
- * */
-export const config = [
-    js.configs.recommended,
-    eslintConfigPrettier,
-    ...ts.configs.recommended,
-    {
-        plugins: {
-            turbo: turboPlugin,
-        },
-        rules: {
-            "turbo/no-undeclared-env-vars": "warn",
-        },
+import path from 'node:path';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import turboPlugin from 'eslint-plugin-turbo';
+import ts from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default ts.config(
+  includeIgnoreFile(path.join(import.meta.dirname, '../../.gitignore')),
+  { ignores: ['**/*.config.*'] },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    plugins: {
+      import: importPlugin,
+      turbo: turboPlugin
     },
-    {
-        plugins: {
-            onlyWarn,
-        },
-    },
-    {
-        ignores: ["dist/", ".wrangler/"],
-    },
-];
+    extends: [
+      js.configs.recommended,
+      ...ts.configs.recommended,
+      ...ts.configs.recommendedTypeChecked,
+      ...ts.configs.stylisticTypeChecked
+    ],
+    rules: {
+      ...turboPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' }
+      ],
+      '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+      '@typescript-eslint/no-unnecessary-condition': [
+        'error',
+        {
+          allowConstantLoopConditions: true
+        }
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level']
+    }
+  },
+  {
+    linterOptions: { reportUnusedDisableDirectives: true },
+    languageOptions: { parserOptions: { projectService: true } }
+  }
+);
--- tooling/eslint/sveltekit.js
+++ tooling/eslint/sveltekit.js
@@ -1,31 +1,30 @@
-// @ts-nocheck
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import ts from 'typescript-eslint';
-import { config as baseConfig } from './base.js'
+import baseConfig from './base.js';
 
 export default ts.config(
-    ...baseConfig,
-	...svelte.configs['flat/recommended'],
-	...svelte.configs['flat/prettier'],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ['**/*.svelte'],
+  ...baseConfig,
+  ...svelte.configs['flat/recommended'],
+  ...svelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte'],
 
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'src/lib/paraglide/']
-	}
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser
+      }
+    }
+  },
+  {
+    ignores: ['build/', '.svelte-kit/', 'src/lib/paraglide/']
+  }
 );
