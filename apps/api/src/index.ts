8c4f7fd3a4bf79e5c2ae508853d0aad1413bf16e
azez <t.s.connect.m@gmail.com>
2024-12-29 08:46:10 +0300

    🧹 chore: ignore triple slash for api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./worker-configuration.d.ts" />
 import { Elysia } from 'elysia';
 import { app } from './routes/app';
