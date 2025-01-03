88b73a7ae0262126ee0fde915f4cd512ef67d07c
azez <t.s.connect.m@gmail.com>
2025-01-03 17:18:35 +0300

    🧹 chore: switch accounts to use turso

--- packages/auth/src/oauth.ts
+++ packages/auth/src/oauth.ts
@@ -1,10 +1,19 @@
 import { Google } from "arctic";
 
-export const createGoogleProvider = (
-  GOOGLE_CLIENT_ID: string,
-  GOOGLE_CLIENT_SECRET: string,
-  urlOrigin: string,
-) => {
+interface Env {
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+}
+
+export const createGoogleProvider = (env: Env, urlOrigin: string) => {
+  const GOOGLE_CLIENT_ID = env.GOOGLE_CLIENT_ID;
+  if (!GOOGLE_CLIENT_ID) {
+    throw new Error("Missing Google client ID");
+  }
+  const GOOGLE_CLIENT_SECRET = env.GOOGLE_CLIENT_SECRET;
+  if (!GOOGLE_CLIENT_SECRET) {
+    throw new Error("Missing Google client secret");
+  }
   return new Google(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
